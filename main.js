fe8f5c881474f7d0cbb9924d51fed7b2228adeac
Max437575 <felder_max@web.de>
2021-08-26 16:26:13 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var canvas = /** @type {HTMLCanvasElement}*/(document.getElementById("canvas"));
+var ctx = canvas.getContext("2d");
+
+
+var player = new Player();
+
+function draw(){
+    ctx.fillStyle = "#bbbbbb";
+    ctx.fillRect(0, 0, 1920, 1080);
+
+    drawBg();
+    drawTrees();
+    
+    //targets
+    drawTargets();
+    
+    drawGrass();
+    drawWaterBg();
+    
+    //ducks
+    drawDucks();
+    
+    drawWater();
+    drawWood();
+    drawStall();
+
+    //gui
+    drawScore();
+    drawHighScore();
+    drawTimer();
+    drawGui();
+
+    //player
+    player.draw();
+
+    drawScoreTexts();
+
+    spawn();
+
+    window.requestAnimationFrame(draw);
+}
+
+
+draw();
